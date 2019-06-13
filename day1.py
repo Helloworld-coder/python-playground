d5a9ae122ad79d7b150bd360b6b045a77591f1f2
Gopinath A K <gopinath4ak@gmail.com>
2019-06-13 14:21:29 +0530

    else added

--- day1.py
+++ day1.py
@@ -4,6 +4,8 @@ def findMyCar(cars,find):
     for car in cars:
         if car == find:
             print ("hey i found you car "+ find)
+        else:
+            print("Oops Car not found, Try other car.")    
                 
 cars=["ford","honda","hyundai","bmw","maruthi","maserati","hummer"]
 findMyCar(cars,"hummer")
