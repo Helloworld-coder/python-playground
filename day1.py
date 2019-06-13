f776e61d6ac3ca8d9d47effd1752f5b908a73159
jana <janardhanperumal@gmail.com>
2019-06-13 14:52:29 +0530

    issue need to be fixed

--- day1.py
+++ day1.py
@@ -7,3 +7,16 @@ def findMyCar(cars,find):
                 
 cars=["ford","honda","hyundai","bmw","maruthi","maserati","hummer"]
 findMyCar(cars,"hummer")
+
+
+def findMyCar(cars,find):
+    for car in cars:
+        if car == find:
+            print ("hey i found you car "+ find)
+        
+        elif car != find:
+            print("car not found") 
+
+cars=["ford","honda","hyundai","bmw","maruthi","maserati","hummer"] 
+findMyCar(cars,"jaguar")  
+
