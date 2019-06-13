fb54b5e113c51359875644f0050c4f8d320772e3
jana <janardhanperumal@gmail.com>
2019-06-13 13:49:18 +0530

    tested loops

---
+++ cars.py
@@ -0,0 +1,11 @@
+def findMyCar(cars,car):
+    for car in cars:
+        if car == "hummer":
+            print ("hey i found it")
+        elif car == "maserati":
+            print ("i found your second car")
+
+cars=["ford","honda","hyundai","bmw","maruthi","maserati","hummer"]:
+findMyCar(cars,"hummer")
+
+    
