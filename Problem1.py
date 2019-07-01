67a6af049fd9133f6346e99e89eff6a0dc35db1e
jana <janardhanperumal@gmail.com>
2019-07-01 14:14:05 +0530

    list methods updated with syntax

--- Problem1.py
+++ Problem1.py
@@ -14,10 +14,50 @@ print(fruits)
 #for fruit in range(1,10,2):
 #    print(fruits[fruit])
 
-"""
+
+
 
 fruits=["apple","orange","papaya","avacado"]
 for r in range(0,3):
     print(fruits[r])
+
+
+#going to assess the speed of the car
+#using loops and boolean
+
+def speedOfTheCar (speed):
+    carspeed=True
+    if speed > 80:
+        carspeed=False
+        return "slow down"
+    else:
+        return "Safe driving"
+
+data=input("speed of the car: ")
+
+#compare the speed of the car
+def compare(car1,car2):
+    if car1 < car2:
+        return "car 1 is efficient"
+    else:
+        return " car 2 is efficiet"
+car1=data
+car2=input("enter the speed of the car2: ")
+
+fruits=["apple","oak","guvavu"]
+for fruit in range(fruits):
+    print(fruits.int([0,1]))
+
+"""
+i=0
+while i<10:
+    print(i)
+    i+=5       # this is equal to i=i+1
+
+# index()
+    # retuns the position or index of the given argument.
+laptopnames=["lenovo","hp","dell","apple"]
+checkPosition=laptopnames.index("dell")
+print(checkPosition)
 
 
--- lists-method.py
+++ lists-method.py
@@ -1,5 +1,96 @@
-#append
+#append()
+    # adds only 1 string to the list
+    
 mobiles=["apple","samsung","sony","nokia","lg"]
-mobiles.append(["lenovo","honor","huwaei"])
+mobiles.append("asus")
 print(mobiles)
 
+    # adds multiple strings when given inside []
+mobiles=["apple","samsung","sony","nokia","lg"]
+mobiles.append(["asus","lenovo"])
+print(mobiles)
+
+# extend
+    # joins the new list to the end of the previous list
+    # differs from append since it joins 2 list whereas append donot join two lists as one.
+cars = list(("hummer","maserati","ferrari","ford"))
+carsModels= ["gt","mustang","murcielago","accord"]
+#cars.extend(carsModels)
+print(cars)
+ 
+
+#insert()
+    # uses index - inserts the item at a specified index
+cars=["bmw","benz","honda","hyundai"]
+cars.insert(1,"hummer")
+totalCars=cars
+print(totalCars)
+
+#remove()
+    # removes an item from the list
+weapons=["guns","knives","swords","bombs","bullets"]
+weapons.remove("knives")
+gadgets=weapons
+print(gadgets)
+
+#pop()
+    # uses index - removes the item from the specified index or the last item - if not specified
+weapons=["guns","knives","swords","bombs","bullets"]
+weapons.pop(1)
+gadgets=weapons
+print(gadgets)
+
+#del
+    # mainly deletes the entire list
+    # uses index - deletes the item from the given index
+    # del list method is used before the list name
+weapons=["guns","knives","swords","bombs","bullets"]
+del weapons[4]
+gadgets=weapons 
+print(gadgets)
+
+# clear()
+    # clears the entire list
+    # no arguments should be given under ()
+pharmabrands=["sanofi","pfizer","cipla","ranbaxy"]
+pharmabrands.clear()
+print(pharmabrands)
+
+# copy()
+    # makes a copy of the list to another list name or variable
+laptopnames=["lenovo","hp","dell","apple"]
+newnames=laptopnames.copy()
+print(newnames)
+
+# list()
+    # makes a copy of the list to another list name or variable
+laptopnames=["lenovo","hp","dell","apple"]
+newnames=laptopnames.copy()
+print(newnames)
+
+# sort()
+    # sorts the list items as per order
+laptopnames=["lenovo","hp","dell","apple"]
+laptopnames.sort()
+reversedname=laptopnames
+print(laptopnames)
+
+# reverse()
+    # reverse the entire list
+laptopnames=["lenovo","hp","dell","apple"]
+reversedname.reverse()
+print(reversedname)
+
+# count()
+    # returns the number of repeats of the given argument, if not returns 0.
+    # should assign to a variable and then returns or print the output.
+laptopnames=["lenovo","hp","dell","apple"]
+checkNames=laptopnames.count("apple")
+print(checkNames)    
+
+# index()
+    # retuns the position or index of the given argument.
+    # should assign to a variable and then returns or print the output.
+laptopnames=["lenovo","hp","dell","apple"]
+checkIndex=laptopnames.index("dell")
+print(checkIndex)
